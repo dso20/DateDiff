09c9a912296b35baca00625d23e960b36abcd364
dso20 <dso20@hotmail.co.uk>
2018-04-16 17:47:11 +0100

    ignore

--- DateDiffMVC/Interfaces/IDecoratorLoggerService.cs
+++ DateDiffMVC/Interfaces/IDecoratorLoggerService.cs
@@ -3,7 +3,6 @@
     //ensuring child class can sub for parent
     public interface IDecoratorLoggerService : ILoggerService
     {
-        void Log(string message, object source);
         void Newfunction(string test);
     }
 }
