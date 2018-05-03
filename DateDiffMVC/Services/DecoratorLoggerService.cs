5f847bd313298284981f6f8f10f9804204d517ab
David Owen <dowen@trilliumsystems.net>
2018-05-03 17:57:14 +0100

    ignore

--- DateDiffMVC/Services/DecoratorLoggerService.cs
+++ DateDiffMVC/Services/DecoratorLoggerService.cs
@@ -2,6 +2,7 @@ namespace DateDiffMVC.Controllers
 {
     //so below can now stand in for above where it's used, but at the same time has provided a newfunction for whatever needs it
     //autofac can account for decorators, so the correct object is passed to the controller
+    //you can also decorate to extend entirely diff class, eg calendar service, that could have a decorator for logging
     public class DecoratorLoggerService : IDecoratorLoggerService
     {
         private readonly ILoggerService _logger;
@@ -13,7 +14,7 @@ namespace DateDiffMVC.Controllers
 
         public void Log(string message, object source)
         {
-            _logger.Log(message,source);
+            _logger.Log(message + " Extra Decorator",source);
         }
 
         public void Newfunction(string test)
